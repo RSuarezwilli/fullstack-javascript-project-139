e0bf9f7b582cc137b9951b1fa05c288f47288925
root <root@DESKTOP-A93JRRE.>
2026-02-09 16:22:06 -0500

    cambios en mi proyecto-139

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,46 +1,14 @@
-import React from 'react';
+// frontend/src/index.js
+// import React from 'react'; // Se elimina si no usamos React directamente
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react";
-import rollbar from "./rollbar"; 
-
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
-
-// Socket.IO
-import { io } from "socket.io-client";
-
-// Conexión al backend
-export const socket = io("http://localhost:5000", {
-  reconnectionAttempts: 5,
-  reconnectionDelay: 1000,
-});
-
-// Logs de conexión
-socket.on("connect", () => {
-  console.log("Cliente conectado:", socket.id);
-});
-
-// Logs de mensajes recibidos
-socket.on("message", (msg) => {
-  console.log("Mensaje recibido:", msg);
-});
+import init from './init.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <RollbarProvider instance={rollbar}>
-    <ErrorBoundary>
-      <Provider store={store}>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </Provider>
-    </ErrorBoundary>
-  </RollbarProvider>
-);
+init().then((vdom) => {
+  root.render(vdom);
+});
 
 reportWebVitals();
