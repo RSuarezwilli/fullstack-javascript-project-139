646ca8894e92af473b3f75ad0205f33c5ac2252d
root <root@DESKTOP-A93JRRE.>
2026-02-09 16:43:52 -0500

    cambios en mi proyecto-139

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,34 +1,20 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../components/Header";
-import Login from "./Login";
-import Signup from "./Signup";
-import Home from "./Home";
-import NotFound from "./NotFound";
-import { AuthProvider } from "../components/AuthContext";
+// frontend/src/components/App.jsx
+import React from 'react';
+import '../App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import AppRoutes from '../routes.js';
+import ChatNavbar from './Navbar/ChatNavbar.jsx';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-const App = () => {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Header />
-
-        
-        <ToastContainer position="top-right" autoClose={3000} />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  );
-};
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <ChatNavbar />
+      <ToastContainer />
+      <AppRoutes />
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
